f39343aad9aa473c162a100d026027d26c04b08d
Gladiator07 <atharvaaingle@gmail.com>
2021-12-29 13:57:51 +0530

    test basic extractive summarizer

--- app.py
+++ app.py
@@ -2,5 +2,21 @@ import streamlit as st
 
 
 if __name__ == "__main__":
-    st.header("Streamlit 🤝 Colab")
+    # adding modules to path
+    import sys
+    sys.path.append("../extractive_summarizer")
+    st.title("Text Summarizer 📝")
     st.write("This is a test statement")
+
+    inp_text = st.text_input("Enter the text here")
+    summarize_type = st.sidebar.selectbox("Summarization type", options=["Extractive", "Abstractive"])
+
+    if summarize_type == "Extractive":
+        from extractive_summarizer import Summarizer
+
+        # init model
+        model = Summarizer()
+        summarized_text = model(inp_text, num_sentences=5)
+
+        st.subheader("Summarized text")
+        st.markdown(summarized_text)
