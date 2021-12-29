e36f01a9f131a566d9568379f9b5999ed6821b2d
Gladiator07 <atharvaaingle@gmail.com>
2021-12-29 15:30:01 +0530

    try reducing run time

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ import streamlit as st
 from extractive_summarizer.model_processors import Summarizer
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 
-def abstractive_summarizer(text : str):
-
-    model = T5ForConditionalGeneration.from_pretrained('t5-large')
-    tokenizer = T5Tokenizer.from_pretrained('t5-large')
-    device = torch.device('cpu')
+def abstractive_summarizer(text : str, model):
 
     preprocess_text = text.strip().replace("\n", "")
     t5_prepared_text = "summarize: " + preprocess_text
@@ -25,7 +21,20 @@ def abstractive_summarizer(text : str):
     return abs_summarized_text
 
 if __name__ == "__main__":
+    # ---------------------
+    # download models
+    # ---------------------
+    abs_model = T5ForConditionalGeneration.from_pretrained('t5-large')
+    tokenizer = T5Tokenizer.from_pretrained('t5-large')
+    device = torch.device('cpu')
 
+    # init extractive summarizer (bad practice, fix later)
+    # init model
+    ext_model = Summarizer()
+    
+    # ---------------------------------
+    # Main Application
+    # ---------------------------------
     st.title("Text Summarizer 📝")
     summarize_type = st.sidebar.selectbox("Summarization type", options=["Extractive", "Abstractive"])
 
@@ -41,12 +50,11 @@ if __name__ == "__main__":
     if summarize:
         if summarize_type == "Extractive":
             # extractive summarizer
-            # init model
-            model = Summarizer()
-            summarized_text = model(inp_text, num_sentences=5)
+            
+            summarized_text = ext_model(inp_text, num_sentences=5)
       
         elif summarize_type == "Abstractive":
-            summarized_text = abstractive_summarizer(inp_text)
+            summarized_text = abstractive_summarizer(inp_text, model=abs_model)
 
         # final summarized output    
         st.subheader("Summarized text")
